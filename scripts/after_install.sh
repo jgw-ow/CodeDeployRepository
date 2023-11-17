84c8d7087670d5940684510670fc54a09155a024
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2023-11-17 17:21:12 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 cd /var/www/html
-sudo mkdir build
+# sudo mkdir build
 
-echo ">>> install node modules ***********************"
-npm install
+# echo ">>> install node modules ***********************"
+# npm install
 
-echo ">>> build project"
-npm run build
+# echo ">>> build project"
+# npm run build
 
 echo ">>> copy build results to web root directory ***"
 mv -f ./build/* ./
 
 echo ">>> remove template files & directories ********"
-rm -rf build src scripts
+# rm -rf build src scripts
+rm -rf scripts build appspec.yml
 
-echo '>>> change owner to ubuntu *********************'
+echo ">>> change owner to ubuntu *********************"
 chown -R ubuntu /var/www/html
