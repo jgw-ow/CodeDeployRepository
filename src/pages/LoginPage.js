c79a3aa006259cd9bde94dede1b83cfc9287fc5e
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2023-11-24 13:49:47 +0900

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -19,7 +19,7 @@ function LoginPage() {
         return;
       } 
 
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+      const response = await fetch(`http://44.201.125.50:5000/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
