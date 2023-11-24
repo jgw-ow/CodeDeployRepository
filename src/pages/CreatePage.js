a77782b37244998efc0ada2b2c36caa39ce49541
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2023-11-24 13:15:10 +0900

    Update CreatePage.js

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -91,7 +91,7 @@ function CreatePage() {
   data.append('itemImage', itemImage);
   data.append('userId',userId )
 
-  axios.post('${process.env.REACT_APP_API_URL}/create', data)
+  axios.post(`${process.env.REACT_APP_API_URL}/create`, data)
     .then(response => {
       alert('경매물품이 등록되었습니다.');
       navigate("/");
